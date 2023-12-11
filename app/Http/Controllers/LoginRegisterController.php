f9730ab097865448108d410c9df4306b496d7eee
sulieman-kh <sul.h.kh@hotmail.com>
2023-12-11 12:01:57 +0300

    add-register-page

--- app/Http/Controllers/LoginRegisterController.php
+++ app/Http/Controllers/LoginRegisterController.php
@@ -6,13 +6,15 @@ use Illuminate\Http\Request;
 
 class LoginRegisterController extends Controller
 {
+    //function for register a new user
+    public function register()
+    {
+        return view('auth.register');
+    }
+    //function for login user
     public function login()
     {
         return view('auth.login');
     }
 
-    public function register()
-    {
-        return view('auth.register');
-    }
 }
